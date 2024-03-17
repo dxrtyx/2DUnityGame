b44549747410cc7e2a6e2de0e2dbc5960a430c50
dxrtyx <dartcomand@gmail.com>
2024-03-17 05:23:57 +0200

    create basic player movement

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float horizontalMove;
+    public int speed; 
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(horizontalMove);
+        PlayerInput();
+        PlayerMove();
+    }
+
+    void PlayerInput()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+    }
+
+    void PlayerMove()
+    {
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
     }
 }
